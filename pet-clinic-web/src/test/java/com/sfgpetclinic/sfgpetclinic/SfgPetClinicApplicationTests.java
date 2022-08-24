69dfbd2bcb34663d1275aa29ad0cafc9dff1ee71
miguelhuerta <guelme88@gmail.com>
2022-08-24 15:25:02 -0700

    fixing breaking unit test. making changes on properties

--- pet-clinic-web/src/test/java/com/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,7 +1,8 @@
-package sfgpetclinic;
+package com.sfgpetclinic.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
