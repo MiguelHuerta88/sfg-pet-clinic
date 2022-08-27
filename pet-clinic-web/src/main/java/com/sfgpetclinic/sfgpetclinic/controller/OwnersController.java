0c4dbf3b41d201417200e234660aab000214ba44
miguelhuerta <guelme88@gmail.com>
2022-08-27 13:03:32 -0700

    Added prefixing to OwnersController to show another way to set up the routes

--- pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/controller/OwnersController.java
@@ -2,11 +2,13 @@ package com.sfgpetclinic.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
-    @GetMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String index() {
         return "owners/index";
     }
