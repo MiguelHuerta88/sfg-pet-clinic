e4c52b5436a7dac54c49c6d14bab1bcd50a8de9b
miguelhuerta <guelme88@gmail.com>
2022-08-28 13:08:18 -0700

    VetsController updates and passing vets to view. Closes #15

--- pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/controller/VetsController.java
@@ -1,13 +1,23 @@
 package com.sfgpetclinic.sfgpetclinic.controller;
 
+import com.sfgpetclinic.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "vets/index", "/vets/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
