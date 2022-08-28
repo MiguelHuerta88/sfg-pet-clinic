0809d803e427d6115ad07a81416001215b40a5db
miguelhuerta <guelme88@gmail.com>
2022-08-28 13:24:01 -0700

    refactored the abstractMap service and all other services save method. We now dont need to pass the ID we can dynamically calculate it. Closes #23

--- pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/bootstrap/DataLoader.java
@@ -20,25 +20,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Miguel");
         owner1.setLastName("Huerta");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Susan");
         owner2.setLastName("Espino");
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Erik");
         owner3.setLastName("Huerta");
         ownerService.save(owner3);
 
         Owner owner4 = new Owner();
-        owner4.setId(4L);
         owner4.setFirstName("Sam");
         owner4.setLastName("Huerta");
         ownerService.save(owner4);
@@ -46,13 +42,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Jorge");
         vet1.setLastName("Camacho");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Macho Man");
         vet2.setLastName("Randy Savage");
         vetService.save(vet2);
