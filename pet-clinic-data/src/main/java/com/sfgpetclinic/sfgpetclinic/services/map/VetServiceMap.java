ac39be631ae3d20f073b9a16f902f4fbda873b29
miguelhuerta <guelme88@gmail.com>
2022-08-30 21:16:54 -0700

    added some similar saving logic to vet service

--- pet-clinic-data/src/main/java/com/sfgpetclinic/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,7 +1,9 @@
 package com.sfgpetclinic.sfgpetclinic.services.map;
 
+import com.sfgpetclinic.sfgpetclinic.model.Specialty;
 import com.sfgpetclinic.sfgpetclinic.model.Vet;
 import com.sfgpetclinic.sfgpetclinic.services.CrudService;
+import com.sfgpetclinic.sfgpetclinic.services.SpecialtyService;
 import com.sfgpetclinic.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -16,6 +24,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialties().size() >0) {
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
