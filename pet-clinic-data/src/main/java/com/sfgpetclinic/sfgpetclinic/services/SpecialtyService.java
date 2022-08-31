7f27e8419d6cb271f51073a47b61bd2e9f63b91e
miguelhuerta <guelme88@gmail.com>
2022-08-30 20:47:24 -0700

    Created Specialty service and interface. Closes #30

---
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.sfgpetclinic.sfgpetclinic.services;
+
+import com.sfgpetclinic.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
