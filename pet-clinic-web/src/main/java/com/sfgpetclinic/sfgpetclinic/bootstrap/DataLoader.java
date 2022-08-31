aa96095563b36efff9474d0d7d65d01e0876129b
miguelhuerta <guelme88@gmail.com>
2022-08-30 20:43:10 -0700

    added contact info and pets for owners in data loader. Closes #32

--- pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.sfgpetclinic.sfgpetclinic.bootstrap;
 
 import com.sfgpetclinic.sfgpetclinic.model.Owner;
+import com.sfgpetclinic.sfgpetclinic.model.Pet;
 import com.sfgpetclinic.sfgpetclinic.model.PetType;
 import com.sfgpetclinic.sfgpetclinic.model.Vet;
 import com.sfgpetclinic.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.sfgpetclinic.sfgpetclinic.services.PetTypeService;
 import com.sfgpetclinic.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,21 +40,46 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Miguel");
         owner1.setLastName("Huerta");
+        owner1.setAddress("1140 E Merced Ave");
+        owner1.setCity("West Covina");
+        owner1.setTelephone("3103676337");
+
+        Pet miguelPet = new Pet();
+        miguelPet.setPetType(savedDog);
+        miguelPet.setOwner(owner1);
+        miguelPet.setBirthDate(LocalDate.now());
+        miguelPet.setName("Metz");
+        owner1.getPets().add(miguelPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Susan");
         owner2.setLastName("Espino");
+        owner2.setAddress("1140 E Merced Ave");
+        owner2.setCity("West Covina");
+        owner2.setTelephone("3234303644");
+        Pet susiePet = new Pet();
+        susiePet.setPetType(savedDog);
+        susiePet.setOwner(owner2);
+        susiePet.setBirthDate(LocalDate.now());
+        susiePet.setName("Chip");
+        owner2.getPets().add(susiePet);
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Erik");
         owner3.setLastName("Huerta");
+        owner3.setAddress("139 E 235th St");
+        owner3.setCity("Carson");
+        owner3.setTelephone("3108306604");
         ownerService.save(owner3);
 
         Owner owner4 = new Owner();
         owner4.setFirstName("Sam");
         owner4.setLastName("Huerta");
+        owner4.setAddress("139 E 235th St");
+        owner4.setCity("Carson");
+        owner4.setTelephone("3108306604");
         ownerService.save(owner4);
 
         System.out.println("Loaded Owners");
