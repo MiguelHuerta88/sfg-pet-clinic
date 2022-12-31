31b82d5907d804e134dd031b94dee0fbeebac060
miguelhuerta <guelme88@gmail.com>
2022-12-30 21:58:11 -0800

    adjusting jdk version in circle ci config

--- pet-clinic-web/src/test/java/com/sfgpetclinic/sfgpetclinic/controller/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/com/sfgpetclinic/sfgpetclinic/controller/OwnersControllerTest.java
@@ -65,7 +65,7 @@ class OwnersControllerTest {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
-        
+
         verifyNoInteractions(ownerService);
     }
 }
